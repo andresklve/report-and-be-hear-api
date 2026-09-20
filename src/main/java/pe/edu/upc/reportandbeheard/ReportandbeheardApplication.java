8c94e0c0f2e33c44a0436eb95afe2807ccd216cc
andresklve <droupandres@gmail.com>
2026-09-20 18:31:31 -0500

    first commit: estructura base del proyecto Spring Boot

---
+++ src/main/java/pe/edu/upc/reportandbeheard/ReportandbeheardApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.reportandbeheard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportandbeheardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportandbeheardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/reportandbeheard/ReportandbeheardApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.reportandbeheard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportandbeheardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
